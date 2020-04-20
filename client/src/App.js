af2b3ffd3133f3fa7c02602da89394f0630adee6
Adonis Puente <adonispuente@Adoniss-Air.attlocal.net>
2020-04-20 19:48:55 -0400

    done no stretch

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import SavedList from './Movies/SavedList';
+import SavedList from "./Movies/SavedList";
+import { Route, Switch, Link } from "react-router-dom";
+// import axios from "axios";
+import MovieList from "./Movies/MovieList";
+import Movie from "./Movies/Movie";
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
 
-  const addToSavedList = movie => {
+  const addToSavedList = (movie) => {
     setSavedList([...savedList, movie]);
   };
 
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Switch>
+        {/* the switch makes it so that only first route encountered inside renders */}
+        <Route path="/movies/:id">
+          <Movie />
+        </Route>
+
+        {/* put the least specific path at the end of the switch */}
+        <Route path="/">
+          <MovieList />
+        </Route>
+      </Switch>{" "}
     </div>
   );
 };
